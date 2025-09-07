a998b4de64e754b4afbb55ade0fd47af07d8148a
User <daun64846@gmail.com>
2025-09-07 17:57:39 +0300

    Fruit

--- kkiri.py
+++ kkiri.py
@@ -1,7 +1,53 @@
-print("Hello World")
+# class Dog:
+#    def data(self, name):
+#    print(name, "Gav", selg.age)
+# self.age=7
 
-print("Hi")
 
-print("Hello World")
+# dog1.data("Bobik", sex "m")
+# dog1 = Dog()
+#
+# dog2 = Dog()
+# dog2.data("Tuzik", sex "w")
 
-print("Hi")
+
+# def data(name):
+#     print(name, "Gav")
+# a = data("Tuzik")
+# age =5
+
+
+# class Dog:
+#    def __init__(self, name, sex):
+#    print(name, "Gav", selg.age)
+# self.age=7
+#
+# dog1 = Dog (name"Tuzik", sex "m")
+#
+# dog2 = Dog(name "Bobita", sex "w"
+
+# class Dog:
+#    def __init__(self, name, age):
+#    self.a = name
+#    self.b = age
+#
+# dog1 = Dog("Tuzik", age = 6)
+#
+# print(self.a)
+# print(self.b)
+
+class Price_fruit:
+
+    def __init__(self, fruit_name):
+        print("Фрукт", fruit_name )
+
+    def price(self, fruit_price):
+        print("Ціна", fruit_price)
+
+
+apple = Price_fruit("Яблуко")
+apple.price("12 грн")
+
+lemon = Price_fruit("Лимон")
+lemon.price("8 грн")
+
